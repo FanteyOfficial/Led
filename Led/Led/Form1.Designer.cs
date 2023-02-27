695642bcc53477c6e18818f95cd55a3231cbeae2
FanteyOfficial <81305405+FanteyOfficial@users.noreply.github.com>
2023-02-27 23:54:46 +0100

    Add files via upload

---
+++ Led/Led/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Led
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.led_Test1 = new Led.Led_Test();
+            this.SuspendLayout();
+            // 
+            // led_Test1
+            // 
+            this.led_Test1.Colore = System.Drawing.Color.Red;
+            this.led_Test1.Dimensione = 10;
+            this.led_Test1.Location = new System.Drawing.Point(161, 63);
+            this.led_Test1.Name = "led_Test1";
+            this.led_Test1.Size = new System.Drawing.Size(11, 11);
+            this.led_Test1.TabIndex = 0;
+            this.led_Test1.Text = "led_Test1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.led_Test1);
+            this.Name = "Form1";
+            this.Text = "Window";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Led_Test led_Test1;
+    }
+}
+
---
+++ Led/Led/Led_Test.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Led
+{
+    internal class Led_Test : Control
+    {
+        Color colore_led = Color.Red;
+        int dime = 10;
+
+        public Led_Test()
+        {
+
+        }
+
+        public Led_Test(Color colore, int dimensione)
+        {
+            dime = dimensione;
+            colore_led = colore;
+        }
+
+        protected override void OnPaint(PaintEventArgs pe)
+        {
+            Width = dime + 1; Height = dime + 1;
+            Graphics g = pe.Graphics;
+            System.Drawing.Brush brush = new SolidBrush(colore_led);
+            Pen penna = new Pen(Color.Black, 1);
+            g.FillEllipse(brush, 0, 0, dime, dime);
+            g.DrawArc(penna, 0, 0, dime, dime, 0, 360);
+            //MessageBox.Show("disegno");
+
+
+        }
+
+        public Color Colore
+        {
+            get
+            {
+                return colore_led;
+            }
+            set
+            {
+                colore_led = value;
+                this.Invalidate();
+            }
+        }
+        public int Dimensione
+        {
+            get
+            {
+                return dime;
+            }
+            set
+            {
+                dime = value;
+                this.Invalidate();
+            }
+        }
+    }
+}
